d9a58825eb0c99f6e1e850caa2917b3303faa5b4
lzq <vanyaliang@foxmail.com>
2024-10-19 22:40:47 +0800

    add two funcions,one is checkbox, the other is localstorage

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import './App.css';
 
 
 const App =()=>{
-  const [todos,setTodos] = useState([]) //拿数组去存储待办事项
+  //const [todos,setTodos] = useState([]) //拿数组去存储待办事项
+  const [todos,setTodos] = useState(()=>{
+    //从localstorage 读取初始值
+    const saved = localStorage.getItem('todos')
+    return saved ? JSON.parse(saved) : [] // 如果saved存在且不是空值就执行 Json.parse();如果不存在就返回[]
+  });
+
   const [inputValue,setInputValue] = useState('') //inputvalue为空字符  
   const [dueTime,setTime] = useState('')
 
+useEffect(()=>{
+  //如果变化，就保存到localstorage中
+  localStorage.setItem('todos',JSON.stringify(todos))
+},[todos])
 
 // 添加新的事项
 const addTodo=()=>{
   if(inputValue.trim()&&dueTime.trim()){
     // 创建新的新代办对象，包含内容喝截止时间
-    const newTodos = {text:inputValue,dueTime:dueTime}
+    const newTodos = {text:inputValue,dueTime:dueTime,completed: false}
     // 更新todos数组，将新的待办事项加入
     setTodos([...todos, newTodos])
     setInputValue('')
@@ -23,6 +33,12 @@ const addTodo=()=>{
 }
 const deleteTodo=(index)=>{
   const newTodos = todos.filter((_,i)=>i !==index)
+  setTodos(newTodos)
+}
+
+const complete = (index)=>{ //点击复选框时触发的函数
+  const newTodos = [...todos]
+  newTodos[index].completed = !newTodos[index].completed
   setTodos(newTodos)
 }
 
@@ -46,6 +62,7 @@ const deleteTodo=(index)=>{
         <table className='table'>
           <thead>
             <tr>
+              <th>完成情况</th>
               <th>待办事项</th>
               <th>截止时间</th>
               <th>操作</th>
@@ -55,6 +72,13 @@ const deleteTodo=(index)=>{
           {/* 遍历todo数组，生成每一行 */}
           {todos.map((toDo,index)=>(
             <tr key={index}>
+              <td>
+                <input 
+                  type='checkbox'
+                  checked={todos[index].completed}
+                  onChange={()=>complete(index)}
+                />
+              </td>
               <td>{toDo.text}</td>
               <td>{toDo.dueTime}</td>
               <td>
